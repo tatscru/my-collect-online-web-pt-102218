c57803e670d3c077dfcec71fcc7756a03f4d6950
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-01 23:17:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,22 @@
 
-# while i < collection.length 
-#   # while i is less than the original array in any length given 
-#     new_collection.upcase << yield(collection[i])
+# def my_collect(languages)
+#   # method being called takes in collection as an arg.
+#   i = [] 
+#   new_collection = []
+#     # here we are iterating over a new collection into a while loop
+#     while i < collection.length 
+#       new_collection << yield(collection[i])
 #   end 
-collection 
+#   collection
 # end 
-
-def my_collect(languages)
-  # method being called takes in collection as an arg.
-  i = [] 
+ 
+ 
+def my_collect(collection)
+  i = 0
   new_collection = []
-    # here we are iterating over a new collection into a while loop
-    while i < collection.length 
-      new_collection << yield(collection[i])
-  end 
-  collection
-end 
- 
- 
-
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
