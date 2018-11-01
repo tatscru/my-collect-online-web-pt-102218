e10c5bd050b356eb939b2c2a2a081d4788103677
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-01 23:11:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(collection)
   # method being called takes in collection as an arg. 
   i = 0
-  new_collection.upcase = []
+  new_collection = []
   # here we are iterating over a new collection into a while loop
  while i < collection.length 
   # while i is less than the original array in any length given 
